9ac09eea1245f59b6d2bc3eb10e3b5eb709e0ef0
balraj singh <balraj.singh@elogist.in>
2018-08-15 11:10:22 +0530

    server add

--- start_script.sh
+++ start_script.sh
@@ -3,4 +3,4 @@ git pull origin master
 python setup.py install
 sudo pkill -f -9 supervisor
 sudo pkill -f -9 python
-sudo -E supervisord -c /root/jobwork.io/jobwork/conf/supervisord.conf
+sudo -E supervisord -c /root/jobwork.io-master-test/jobwork/conf/supervisord.conf
