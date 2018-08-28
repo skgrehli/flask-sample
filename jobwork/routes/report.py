9b6037c77cf242c003969e5f4fd6ef93a8b93542
balraj singh <balraj.singh@elogist.in>
2018-08-28 17:59:52 +0530

    wish list

--- jobwork/routes/report.py
+++ jobwork/routes/report.py
@@ -6,6 +6,7 @@ from jobwork.utils.common_utils import CommonUtils
 from datetime import datetime
 from jobwork.models.jobcomments import JobComments
 from jobwork.models.jobs import Jobs
+from jobwork.db_connection import db
 
 jw_report=Blueprint('jw_report',__name__,url_prefix='')
 
@@ -88,4 +89,29 @@ def reportjob():
     else:
         return jsonify({"status": 400, "message": "No Data Recevied.","error":True,"response":[]})
 
+@jw_report.route('/user/job/wishlist/create', methods=['POST'])
+@authentication
+def addjobtowishlist():
+    userid = int(request.json['userid'])
+    token = request.json['token']
+    jobid = request.json['jobid']
+    data=db.wishlist.count({"userid":userid,"jobid":jobid})
+    print(data)
+    if data==0:
+        db.wishlist.insert({"userid":userid,"jobid":jobid,"addtime":datetime.now(),"action":False})
+        return jsonify({"status": 200, "message": "job added to wishlist", "error": False, "response": 0})
+    else:
+        return jsonify({"status": 200, "message": "job already in wish list ", "error": False, "response": 1})
 
+@jw_report.route('/user/job/wishlist/remove', methods=['POST'])
+@authentication
+def removejobtowishlist():
+    userid = int(request.json['userid'])
+    token = request.json['token']
+    jobid = request.json['jobid']
+    data=db.wishlist.count({"userid":userid,"jobid":jobid})
+    if data==0:
+        return jsonify({"status": 200, "message": "job not found in list", "error": False, "response": 0})
+    else:
+        db.wishlist.remove({"userid": userid, "jobid": jobid})
+        return jsonify({"status": 200, "message": "job removed from wish list ", "error": False, "response": 1})
